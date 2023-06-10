5a7b617ec27a08893aa3a843d0aa89f57da5e934
Franciscot93 <franciscot.p.93@gmail.com>
2023-06-10 01:48:08 -0300

    reduccion de lineas de codigo, intento de clean code

--- ejercicioClase6jS.js
+++ ejercicioClase6jS.js
@@ -1,253 +1,171 @@
 const nameUser=document.getElementById('nameUser');
-
 const lastNameUser=document.getElementById('lastNameUser');
-
 const emailUser=document.getElementById('emailUser');
-
 const passwordUser=document.getElementById('passUser');
-
-const repeatPasswordUser=document.getElementById('rePassUser');
-
+const rePasswordUser=document.getElementById('rePassUser');
 const birthdayDateUser=document.getElementById('birthdayUser');
-
 const newsletterUserOption=document.getElementById('newsletterUser');
-
-
 const registerButton=document.getElementById('buttonRegister');
 registerButton.disabled=true;
-
 const registerForm=document.getElementById('registerForm');
+const formInputs=document.querySelectorAll('input');
+
+
 
 const inputFields={
-  name:false,
-  lastName:false,
-  email:false,
-  password:false,
-  confirmPassword:false,
-  ageUser:false,
+  nameUser:false,
+  lastNameUser:false,
+  emailUser:false,
+  passwordUser:false,
+  rePasswordUser:false,
+  birthdayUser:false,
+  currentAge:0
 
 }
 
 const validationRules = {
-    name: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, //letras y espacios, pueden llevar acentos.
-    password: /^.{8,16}$/, //8 a 16 digitos
-    email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-    age:18
-  };
-
-
-const nameValidation=(validationRules,nameUser)=>{
-    const validation= (validationRules.name).test(nameUser.value)
-
-    return validation
-}
-const lastNameValidation=(validationRules,lastNameUser)=>{
-  const validation=(validationRules.name).test(lastNameUser.value)
-
-  return validation
+    nameUser: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, //letras y espacios, pueden llevar acentos.
+    lastNameUser: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
+    passwordUser: /^.{8,16}$/, //8 a 16 digitos
+    emailUser: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 }
 
-const emailUserValidation=(validationRules,emailUser)=>{
-  const validation=(validationRules.email).test(emailUser.value)
 
-  return validation
-}
-
-const passwordUserValidation=(validationRules,passwordUser)=>{
-  const validation=(validationRules.password).test(passwordUser.value)
-
-  return validation
-}
-
-const repeatPasswordUserValidation=(passwordUser,repeatPasswordUser)=>{
-  if(passwordUser.value === repeatPasswordUser.value){
-    return true
+  const rePasswordUserValidation=(passwordUser,rePasswordUser)=>{
+    if(passwordUser.value === rePasswordUser.value){
+      return true
+    }
+    return false
   }
-  return false
-}
 
-const birthdayDateUserValidation=(birthdayDateUser)=>{
-  const birthDayToString=String(birthdayDateUser.value)
-  const today= new Date()
-  const todayYear=parseInt(today.getFullYear());
-  const todayMonth=parseInt(today.getMonth())+1;
-  const todayDay=parseInt(today.getDate());
-
-  const birthdayDateYear= parseInt(birthDayToString.substring(0,4));
-  const birthdayDateMonth= parseInt(birthDayToString.substring(6,7));
-  const birthdayDateDay= parseInt(birthDayToString.substring(8,10));
+  const birthdayUserValidation=(input)=>{
+    const birthDayToString=String(input.value)
+    const today= new Date()
+    const todayYear=parseInt(today.getFullYear());
+    const todayMonth=parseInt(today.getMonth())+1;
+    const todayDay=parseInt(today.getDate());
   
-
- 
-
- const ageUser= todayYear - birthdayDateYear;
-
-  if(todayMonth<birthdayDateMonth){
-    ageUser--;
-  }
-  if(todayMonth===birthdayDateMonth){
-    if(todayDay<birthdayDateDay){
+    const birthdayDateYear= parseInt(birthDayToString.substring(0,4));
+    const birthdayDateMonth= parseInt(birthDayToString.substring(6,7));
+    const birthdayDateDay= parseInt(birthDayToString.substring(8,10));
+    
+  
+   
+  
+   const ageUser= todayYear - birthdayDateYear;
+  
+    if(todayMonth<birthdayDateMonth){
       ageUser--;
     }
+    if(todayMonth===birthdayDateMonth){
+      if(todayDay<birthdayDateDay){
+        ageUser--;
+      }
+    }
+  
+    if (ageUser>=18){
+      return ageUser
+    }
+    if (ageUser<18){
+      return false
+    }
+    
   }
+const inputValidation=(validationRules,input)=>{
 
-  return ageUser
+    if(input.name==='birthdayUser'){
+      const ageUser=birthdayUserValidation(input)
+      inputFields[input.name]=true
+      inputFields.currentAge=ageUser
+
+      return ageUser;
+    }
+    if(input.name==='rePasswordUser' && passwordUser.value === input.value ){
+      inputFields[input.name]=true
+      return true
+      
+    }
+    
+    if(!(input.name==='birthdayUser') && !(input.name==='rePasswordUser')){
+    
+      const validation=(validationRules[input.name]).test(input.value)
+   
+      
+    
+      return validation
+    }}
+
+
+
+
+
+const validationSystem=(input,validationRules)=>
+ {
+    inputValidation(validationRules,input)
+    if(inputValidation(validationRules,input)){
+        document.getElementById(`${input.name}Fine`).classList.remove('itsFineInput')
+        document.getElementById(`${input.name}Fine`).classList.add('itsFineInputActive')
+        document.getElementById(`${input.name}Error`).classList.add('errorInput')
+        document.getElementById(`${input.name}Error`).classList.remove('errorInputActive')
+        inputFields[input.name]=true
+        console.log(inputFields[input.name]);
+    }
+    if(!inputValidation(validationRules,input)){
+        document.getElementById(`${input.name}Fine`).classList.remove('itsFineInputActive')
+        document.getElementById(`${input.name}Fine`).classList.add('itsFineInput')
+        document.getElementById(`${input.name}Error`).classList.remove('errorInput')
+        document.getElementById(`${input.name}Error`).classList.add('errorInputActive')
+        
+    if(input.value=='' ){
+      document.getElementById(`${input.name}Error`).classList.add('errorInput')
+      document.getElementById(`${input.name}Error`).classList.remove('errorInputctive')
+      document.getElementById(`${input.name}Fine`).classList.remove('itsFineInputActive')
+      document.getElementById(`${input.name}Fine`).classList.add('itsFineInput')
+      
+    }
+  }
 }
 
-  nameUser.addEventListener('keyup',(e)=>{
-    nameValidation(validationRules,nameUser)
-    if(nameValidation(validationRules,nameUser)){
-        document.getElementById('nameFine').classList.remove('itsFineName')
-        document.getElementById('nameFine').classList.add('itsFineNameActive')
-        document.getElementById('nameError').classList.add('errorName')
-        document.getElementById('nameError').classList.remove('errorNameActive')
-        inputFields.name=true
-    }
-    if(!nameValidation(validationRules,nameUser) | nameUser.value=='' ){
-        document.getElementById('nameFine').classList.remove('itsFineNameActive')
-        document.getElementById('nameFine').classList.add('itsFineName')
-        document.getElementById('nameError').classList.remove('errorName')
-        document.getElementById('nameError').classList.add('errorNameActive')
-        inputFields.name=false;
-    }
-    if(nameUser.value==''){
-      document.getElementById('nameError').classList.add('errorName')
-      document.getElementById('nameError').classList.remove('errorNameActive')
-      document.getElementById('nameFine').classList.remove('itsFineNameActive')
-      document.getElementById('nameFine').classList.add('itsFineName')
-      inputFields.name=false;
-    }
-  })
+formInputs.forEach(input=> input.addEventListener('keyup',(e)=>{
+  validationSystem(input,validationRules)
+}))
+ 
 
+const users=[]
+const registerUser=(nameUser, lastNameUser,emailUser,passwordUser,birthdayDateUser)=>{
 
-  lastNameUser.addEventListener('keyup',(e)=>{
-    lastNameValidation(validationRules,lastNameUser)
-    if(lastNameValidation(validationRules,lastNameUser)){
-        document.getElementById('lastNameFine').classList.remove('itsFineLastName')
-        document.getElementById('lastNameFine').classList.add('itsFineLastNameActive')
-        document.getElementById('lastNameError').classList.add('errorLastName')
-        document.getElementById('lastNameError').classList.remove('errorLastNameActive')
-        inputFields.lastName=true
+  const newUser={
+    Name:nameUser.value,
+    lastName:lastNameUser.value,
+    email:emailUser.value,
+    password:passwordUser.value,
+    age:inputFields.currentAge
     }
-    if(!lastNameValidation(validationRules,lastNameUser) | lastNameUser.value=='' ){
-        document.getElementById('lastNameFine').classList.remove('itsFineLastNameActive')
-        document.getElementById('lastNameFine').classList.add('itsFineLastName')
-        document.getElementById('lastNameError').classList.remove('errorLastName')
-        document.getElementById('lastNameError').classList.add('errorLastNameActive')
-        inputFields.lastName=false
-    }
-    if(lastNameUser.value==''){
-      document.getElementById('lastNameError').classList.add('errorLastName')
-      document.getElementById('lastNameError').classList.remove('errorLastNameActive')
-      document.getElementById('lastNameFine').classList.remove('itsFineLastNameActive')
-      document.getElementById('lastNameFine').classList.add('itsFineLastName')
-      inputFields.lastName=false
-    }
-  })
 
+  return newUser;
+}
 
-  emailUser.addEventListener('keyup',(e)=>{
-    emailUserValidation(validationRules,emailUser)
-    if(emailUserValidation(validationRules,emailUser)){
-        document.getElementById('emailFine').classList.remove('itsFineEmail')
-        document.getElementById('emailFine').classList.add('itsFineEmailActive')
-        document.getElementById('emailError').classList.add('errorEmail')
-        document.getElementById('emailError').classList.remove('errorEmailActive')
-        inputFields.email=true
-    }
-    if(!emailUserValidation(validationRules,emailUser) | emailUser.value=='' ){
-        document.getElementById('emailFine').classList.remove('itsFineEmailActive')
-        document.getElementById('emailFine').classList.add('itsFineEmail')
-        document.getElementById('emailError').classList.remove('errorEmail')
-        document.getElementById('emailError').classList.add('errorEmailActive')
-        inputFields.email=false
-    }
-    if(emailUser.value==''){
-      document.getElementById('emailError').classList.add('errorEmail')
-      document.getElementById('emailError').classList.remove('errorEmailActive')
-      document.getElementById('emailFine').classList.remove('itsFineEmailActive')
-      document.getElementById('emailFine').classList.add('itsFineEmail')
-      inputFields.email=false
-    }
-  })
+registerForm.addEventListener('change',(e)=>{
+  e.preventDefault()
 
+  if(inputFields.nameUser && inputFields.lastNameUser&& inputFields.emailUser&& inputFields.passwordUser && inputFields.rePasswordUser && inputFields.birthdayUser){
+    registerButton.disabled=false;
+  }
 
-  passwordUser.addEventListener('keyup',(e)=>{
-    passwordUserValidation(validationRules,passwordUser)
-    if(passwordUserValidation(validationRules,passwordUser)){
-        document.getElementById('passFine').classList.remove('itsFinePass')
-        document.getElementById('passFine').classList.add('itsFinePassActive')
-        document.getElementById('passwordError').classList.add('errorPassword')
-        document.getElementById('passwordError').classList.remove('errorPasswordActive')
-        inputFields.password=true
-    }
-    if(!passwordUserValidation(validationRules,passwordUser) | passwordUser.value=='' ){
-        document.getElementById('passFine').classList.remove('itsFinePassActive')
-        document.getElementById('passFine').classList.add('itsFinePass')
-        document.getElementById('passwordError').classList.remove('errorPassword')
-        document.getElementById('passwordError').classList.add('errorPasswordActive')
-        inputFields.password=false
-    }
-    if(passwordUser.value==''){
-      document.getElementById('passwordError').classList.add('errorPassword')
-      document.getElementById('passwordError').classList.remove('errorPasswordActive')
-      document.getElementById('passFine').classList.remove('itsFinePassActive')
-      document.getElementById('passFine').classList.add('itsFinePass')
-      inputFields.password=false
-    }
-  })
+})
 
-  repeatPasswordUser.addEventListener('keyup',(e)=>{
-    repeatPasswordUserValidation(passwordUser,repeatPasswordUser)
-    if(repeatPasswordUserValidation(passwordUser,repeatPasswordUser)){
-        document.getElementById('rePassFine').classList.remove('itsFineRePass')
-        document.getElementById('rePassFine').classList.add('itsFineRePassActive')
-        document.getElementById('rePasswordError').classList.add('errorRePassword')
-        document.getElementById('rePasswordError').classList.remove('errorRePasswordActive')
-        inputFields.confirmPassword=true
-    }
-    if(!repeatPasswordUserValidation(passwordUser,repeatPasswordUser) | repeatPasswordUser.value=='' ){
-        document.getElementById('rePassFine').classList.remove('itsFineRePassActive')
-        document.getElementById('rePassFine').classList.add('itsFineRePass')
-        document.getElementById('rePasswordError').classList.remove('errorRePassword')
-        document.getElementById('rePasswordError').classList.add('errorRePasswordActive')
-        inputFields.confirmPassword=false
-    }
-    if(repeatPasswordUser.value==''){
-      document.getElementById('rePasswordError').classList.add('errorRePassword')
-      document.getElementById('rePasswordError').classList.remove('errorRePasswordActive')
-      document.getElementById('rePassFine').classList.remove('itsFineRePassActive')
-      document.getElementById('rePassFine').classList.add('itsFineRePass')
-      inputFields.confirmPassword=false
-    }
-  })
-
-  birthdayDateUser.addEventListener('change',(e)=>
-  { 
-    birthdayDateUserValidation(birthdayDateUser)
-    if(birthdayDateUserValidation(birthdayDateUser)>=validationRules.age){
-        document.getElementById('birthdayFine').classList.remove('itsFineBirdthday')
-        document.getElementById('birthdayFine').classList.add('itsFineBirdthdayActive')
-        document.getElementById('birthdayError').classList.add('errorBirthday')
-        document.getElementById('birthdayError').classList.remove('errorBirthdayActive')
-        inputFields.ageUser=true
-    }
-    if(birthdayDateUserValidation(birthdayDateUser)<validationRules.age | birthdayDateUser.value=='' ){
-        document.getElementById('birthdayFine').classList.remove('itsFineBirdthdayActive')
-        document.getElementById('birthdayFine').classList.add('itsFineBirdthday')
-        document.getElementById('birthdayError').classList.remove('errorBirthday')
-        document.getElementById('birthdayError').classList.add('errorBirthdayActive')
-        inputFields.ageUser=false
-    }
-    if(birthdayDateUser.value==''){
-      document.getElementById('birthdayError').classList.add('errorBirthday')
-      document.getElementById('birthdayError').classList.remove('errorBirthdayActive')
-      document.getElementById('birthdayFine').classList.remove('itsFineBirdthdayActive')
-      document.getElementById('birthdayFine').classList.add('itsFineBirdthday')
-      inputFields.ageUser=false
-    }
-  })
-
+registerButton.addEventListener('click', (e)=>{
+  e.preventDefault()
+  users.push(registerUser(nameUser, lastNameUser,emailUser,passwordUser))
 
 
   
+ 
+})
+
+ console.log(users);
+
+
+
+
+ 
