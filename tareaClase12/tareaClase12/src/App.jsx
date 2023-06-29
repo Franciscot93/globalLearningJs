d673251b2c9d0f491c91198119e1ec9ec28341fc
Franciscot93 <franciscot.p.93@gmail.com>
2023-06-28 23:52:06 -0300

    primer intento de hacer que funcione

--- tareaClase12/tareaClase12/src/App.jsx
+++ tareaClase12/tareaClase12/src/App.jsx
@@ -1,35 +1,104 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef, useState } from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  
+
+  const [isLoading, setIsLoading] = useState(null);
+
+  const handleClick=()=>setIsLoading(false)
+  useEffect(() => {
+    const fetchData = async () => {
+      
+
+      try {
+        const peticion = await fetch(
+          "https://jsonplaceholder.typicode.com/posts");
+        const response = await peticion.json();
+        setData(response);
+
+        if (!response.ok) {
+          console.log("Error al realizar la solicitud");
+          
+          return;
+        }
+
+        
+        console.log(data)
+      } catch (error) {
+        console.log("Error al realizar la solicitud:", error.message);
+        setError("Error al cargar los datos. Por favor, intenta nuevamente.");
+      } finally {
+        setTimeout(() => {
+          setIsLoading(true);
+        }, 5000);
+      }
+    };
+    fetchData()
+   
+  }, [isLoading]);
+
+  if (!isLoading) {
+    return (
+      <>
+        <button onClick={handleClick}>Buscar</button>
+        <button >Cancelar</button>
+        <div>
+          <h1>Publicaciones:</h1>
+          <ul>
+          <p>Cargando...</p>
+          </ul>
+        </div>
+
+        
+      </>
+    );
+  }
+  if (error) {
+    return (
+      <>
+        <button onClick={handleClick}>Buscar</button>
+        <button >Cancelar</button>
+        <p>{error}</p>
+      </>
+    );
+  }
+  if(isLoading && !error && isLoading!=null && isLoading!=false){
+    return (
+      <>
+      
+      <button onClick={handleClick}>Buscar</button>
+        <button >Cancelar</button>
+        <div>
+          <h1>Publicaciones:</h1>
+          <ul>
+            {data.map(post=>(
+              <li key={post.id}>{post.title}</li>
+            ))}
+          </ul>
+        </div>
+      </>
+    )
+  }
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+      <button onClick={handleClick}>Buscar</button>
+        <button >Cancelar</button>
+        <div>
+          <h1>Publicaciones:</h1>
+          <ul>
+          
+          </ul>
+        </div>
+     
     </>
-  )
+  );
 }
 
-export default App
+export default App;
